ac74f4ed26686eb90b1587a9f9b16996e0065199
Alexandre <ab.alexandre.bernier@gmail.com>
2021-05-20 14:57:11 -0400

    Updated simulation python test script to Python3

--- robotiq_85_simulation/robotiq_85_gazebo/scripts/robotiq_85_moveit_test.py
+++ robotiq_85_simulation/robotiq_85_gazebo/scripts/robotiq_85_moveit_test.py
@@ -16,7 +16,7 @@ def robotiq_85_moveit_test():
     ## CALL_SUB_TUTORIAL imports
     ##
     ## First initialize moveit_commander and rospy.
-    print "============ Starting tutorial setup"
+    print("============ Starting tutorial setup")
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('robotiq_85_moveit_test',
                   anonymous=True)
@@ -60,20 +60,20 @@ def robotiq_85_moveit_test():
     ## ^^^^^^^^^^^^^^^^^^^^^^^^^
     ##
     ## We can get the name of the reference frame for this robot
-    print "============ Reference frame: %s" % group.get_planning_frame()
+    print("============ Reference frame: %s" % group.get_planning_frame())
 
     ## We can also print the name of the end-effector link for this group
-    print "============ Reference frame: %s" % group.get_end_effector_link()
+    print("============ Reference frame: %s" % group.get_end_effector_link())
 
     ## We can get a list of all the groups in the robot
-    print "============ Robot Groups:"
-    print robot.get_group_names()
+    print("============ Robot Groups:")
+    print(robot.get_group_names())
 
     ## Sometimes for debugging it is useful to print the entire state of the
     ## robot.
-    print "============ Printing robot state"
-    print robot.get_current_state()
-    print "============"
+    print("============ Printing robot state")
+    print(robot.get_current_state())
+    print("============")
     
     group.set_planner_id("RRTConnectkConfigDefault")
 
@@ -82,7 +82,7 @@ def robotiq_85_moveit_test():
     ## ^^^^^^^^^^^^^^^^^^^^^^^
     ## We can plan a motion for this group to a desired pose for the 
     ## end-effector
-    print "============ Generating plan 1"
+    print("============ Generating plan 1")
     x = 0
     while x < 100 and not rospy.is_shutdown():
         group.set_random_target()
@@ -96,21 +96,21 @@ def robotiq_85_moveit_test():
         x+=1
 
 
-    print "============ Waiting while RVIZ displays plan1..."
+    print("============ Waiting while RVIZ displays plan1...")
     rospy.sleep(5)
 
 
     ## You can ask RVIZ to visualize a plan (aka trajectory) for you.  But the
     ## group.plan() method does this automatically so this is not that useful
     ## here (it just displays the same trajectory again).
-    print "============ Visualizing plan1"
+    print("============ Visualizing plan1")
     display_trajectory = moveit_msgs.msg.DisplayTrajectory()
 
     display_trajectory.trajectory_start = robot.get_current_state()
     display_trajectory.trajectory.append(plan1)
     display_trajectory_publisher.publish(display_trajectory);
 
-    print "============ Waiting while plan1 is visualized (again)..."
+    print("============ Waiting while plan1 is visualized (again)...")
     rospy.sleep(5)
 
 
@@ -135,7 +135,7 @@ def robotiq_85_moveit_test():
 
     ## END_TUTORIAL
 
-    print "============ STOPPING"
+    print("============ STOPPING")
 
 
 if __name__=='__main__':
